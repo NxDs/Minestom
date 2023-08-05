c1457cc2dc33e327ff8131edaac59c3171434547
mworzala <mattheworzala@gmail.com>
2023-08-05 14:07:34 -0400

    hollow-cube/1_20-advancement-packet-fix
    (cherry picked from commit 1f87f26b4f4dcd8c6b3f427f8e47379f1510699f)

--- src/main/java/net/minestom/server/network/packet/server/play/AdvancementsPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/AdvancementsPacket.java
@@ -99,7 +99,8 @@ public record AdvancementsPacket(boolean reset, @NotNull List<AdvancementMapping
 
     public record Advancement(@Nullable String parentIdentifier, @Nullable DisplayData displayData,
                               @NotNull List<String> criteria,
-                              @NotNull List<Requirement> requirements) implements NetworkBuffer.Writer, ComponentHolder<Advancement> {
+                              @NotNull List<Requirement> requirements,
+                              boolean sendTelemetryData) implements NetworkBuffer.Writer, ComponentHolder<Advancement> {
         public Advancement {
             criteria = List.copyOf(criteria);
             requirements = List.copyOf(requirements);
@@ -109,7 +110,8 @@ public record AdvancementsPacket(boolean reset, @NotNull List<AdvancementMapping
             this(reader.read(BOOLEAN) ? reader.read(STRING) : null,
                     reader.read(BOOLEAN) ? new DisplayData(reader) : null,
                     reader.readCollection(STRING),
-                    reader.readCollection(Requirement::new));
+                    reader.readCollection(Requirement::new),
+                    reader.read(BOOLEAN));
         }
 
         @Override
@@ -118,6 +120,7 @@ public record AdvancementsPacket(boolean reset, @NotNull List<AdvancementMapping
             writer.writeOptional(displayData);
             writer.writeCollection(STRING, criteria);
             writer.writeCollection(requirements);
+            writer.write(BOOLEAN, sendTelemetryData);
         }
 
         @Override
@@ -127,7 +130,7 @@ public record AdvancementsPacket(boolean reset, @NotNull List<AdvancementMapping
 
         @Override
         public @NotNull Advancement copyWithOperator(@NotNull UnaryOperator<Component> operator) {
-            return this.displayData == null ? this : new Advancement(this.parentIdentifier, this.displayData.copyWithOperator(operator), this.criteria, this.requirements);
+            return this.displayData == null ? this : new Advancement(this.parentIdentifier, this.displayData.copyWithOperator(operator), this.criteria, this.requirements, this.sendTelemetryData);
         }
     }
 
